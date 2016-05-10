fec7bc11c7f9e51a1a102c9d00039191af67bfb8
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-05-10 13:21:32 -0700

    used local strategy to handle user already signed up - but needs token

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,7 +6,11 @@ function tokenForUser(user){
   const timestamp  = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
+exports.signin = function(req, res, next){
+  //user has already had thier email and password auth'd - just give them a tokenForUser
+  res.send({ token: tokenForUser(req.user) });
 
+};
 exports.signup = function(req, res, next){
   const email  = req.body.email;
   const password  = req.body.password;
--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,24 @@ const User  = require('../models/user');
 const config  = require('../config/index');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+const localOptions = {usernameField: 'email'};
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done){
+  // verify this email and password  - then call done with result
+  User.findOne({ email: email }, function(err, user){
+    if (err){ return done(err);}
+    if (!user){ return done(null, false);}
+    // compare passwords here using bcrypt
+    user.comparePassword(password, function(err, isMatch){
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+      // this will be assigned to req.user in the done callback
+      return done(null, user);
+    });
+  });
+});
+
 
 // set up options
 const jwtOptions = {
@@ -28,3 +46,4 @@ const jwtLogin  = new JwtStrategy(jwtOptions, function(payload, done){
 
 // tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
